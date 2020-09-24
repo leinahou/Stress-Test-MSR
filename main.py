778ea936ec31693f8cb72fbb9bb733d77cb10e4f
Leina <68298361+leinahou@users.noreply.github.com>
2020-09-24 16:58:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import serial, time
+import serial, time     #need to install pyserial
 
-ser = serial.Serial('COM12', 9600)
-#while ser.read(5)!= 'READY': pass
-#ser.write('4012000033330026=20121011000012345678$')
-
-while (1):
-    ser.write('371449635398431=1812941116203$'.encode())
-    print(ser.readline())
-    time.sleep(3)
-
-ser.close()
+ser = serial.Serial('COM12', 9600)   #put your COM port for the arduino
+#while ser.read(5) != 'READY': pass
+i = 0
+while 1:
+    try:
+        ser.write('371449635398431=1812941116203$'.encode('utf-8'))   #put your track2 data before $
+        print(ser.read(ser.inWaiting()), i)
+        time.sleep(3)     #time delay bewteen each write data
+    except Exception as e:
+        print("error: ", e)
+    i = i + 1
+#ser.close()
